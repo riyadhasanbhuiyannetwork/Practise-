90af37e2f3fd9ac6f29c4045838718252a92ebb5
Riyad Hasan Bhuiyan <riyadhasan4m1414@gmail.com>
2026-09-20 18:14:19 +0600

    code

--- 16_area_of_triangle.cpp
+++ 16_area_of_triangle.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    double base, height;
-
-    cout << "Enter base of triangle: ";
-    cin >> base;
-    cout << "Enter height of triangle: ";
-    cin >> height;
-
-double area = 0.5 * base * height;
-    cout << "Area of triangle = " << area << endl;
+int main()
+{
+    float b,h,area;
+    cout<<"B and H:";
+    cin>>b>>h;
+    area = 0.5*b*h;
+    cout << "Area of triangle = " << area;
     return 0;
 }
--- 17_area_of_rectangle.cpp
+++ 17_area_of_rectangle.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    
-    
-    double length, width;
-
- cout << "Enter length of rectangle: ";
-    cin >> length;
- cout << "Enter width of rectangle: ";
-    cin >> width;
-
-    double area = length * width;
- cout << "Area of rectangle = " << area << endl;
+int main()
+{
+    float l,w,area;
+    cout<<"L and W:";
+    cin>>l>>w;
+    area=l*w;
+    cout << "Area of rectangle = " << area;
     return 0;
 }
